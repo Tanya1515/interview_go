51517ca46568585fdc07b8ccb12e8e712ea69e34
Tanya <tanya1515@ispras.ru>
2025-06-08 17:27:45 +0300

    add comments and intro for array and slices

--- arrays_and_slices/range_by_array/range_by_array_comparison/comparison_test.go
+++ arrays_and_slices/range_by_array/range_by_array_comparison/comparison_test.go
@@ -2,13 +2,19 @@ package main
 
 import "testing"
 
+// Есть несколько способов, которые позволяют модифицировать значения 
+// внутри массива. 
+
 // go test -bench=. comparison_test.go
 
+// Есть некоторая структура. 
 type account struct {
 	balance int
 }
 
 func BenchmarkWithPointers(b *testing.B) {
+	// массив accounts может хранить не просто значения
+	// а указатели на стрктуры. 
 	accounts := [...]*account{
 		{balance: 100},
 		{balance: 200},
@@ -18,6 +24,7 @@ func BenchmarkWithPointers(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for _, a := range accounts {
+			// здесь будет происходить обращение по указателю
 			a.balance += 1
 		}
 	}
@@ -33,6 +40,13 @@ func BenchmarkWithIndices(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		for i := range accounts {
+			// здесь модификация происходит по индексу 
+			// то есть есть некоторое начало массива - accounts,
+			// известно, какое количество данных в массиве,
+			// а также их тип. Таким образом, при обращении к элементу по индексу
+			// происходит обращение в память: accounts + i*(размер типа элемента)
+			// Причем этот способ будет работать быстрее, поскольку гораздо проще
+			// итеративно ходить по памяти, чем прыгать по указателям в памяти. 
 			accounts[i].balance += 1
 		}
 	}
--- arrays_and_slices/range_by_slice/range_task/main.go
+++ arrays_and_slices/range_by_slice/range_task/main.go
@@ -4,10 +4,15 @@ func main() {
 	var x = []string{"A", "M", "C"}
 
 	for i, s := range x {
-		println(i, s)
+		// 1 - А, 2 - М, 3 - С, поскольку 
+		// срез s будет скопирован в range. 
+		println(i, s) 
 
 		x[i+1] = "M"
 		x = append(x, "Z")
 		x[i+1] = "Z"
 	}
+
+	// Однако, исходный срез будет модифицирован
+	// [ "A", "Z", "Z", "Z", "Z", "Z" ]
 }
