bbca08c4ecd40c1a4edb66fd1965672f3bf58871
Tanya1515 <tanyaozerova1318@gmail.com>
2025-07-14 13:12:08 +0300

    add comments for goroutines section

--- goroutines_and_scheduler/goroutines_number/main.go
+++ goroutines_and_scheduler/goroutines_number/main.go
@@ -5,6 +5,7 @@ import (
 	"runtime"
 )
 
+// Функция main тоже считается отдельной горутиной.
 func main() {
-	fmt.Printf("Goroutines: %d\n", runtime.NumGoroutine())
+	fmt.Printf("Goroutines: %d\n", runtime.NumGoroutine()) // Goroutines: 1
 }
--- goroutines_and_scheduler/goroutines_with_panic/tcp_server_with_panic/main.go
+++ goroutines_and_scheduler/goroutines_with_panic/tcp_server_with_panic/main.go
@@ -9,12 +9,17 @@ import (
 // nc 127.0.0.1 12345
 
 func main() {
+
+	// здесь работает некоторый listener, который слушает на tcp-порту 12345
 	listener, err := net.Listen("tcp", ":12345")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
+		// функция Accept ждет некоторого клиента, а затем возвращает connection
+		// c этим клиентом. После этого вызывается функция, в рамках
+		// которой отрабатывает паника. При этом упадет не одна горутина, а все приложение.
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Println(err)
